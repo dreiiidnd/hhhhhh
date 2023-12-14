227b08bb93a7628a05dc40f6310a503dc8486325
dreiiidnd <153768430+dreiiidnd@users.noreply.github.com>
2023-12-14 15:32:10 +0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main() {
+    std::cout << "I sincerely apologize for any inconvenience or misunderstanding. We are committed to making things right.\n";
+
+    return 0;
+}
